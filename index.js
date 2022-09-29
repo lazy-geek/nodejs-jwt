b7c1ccf689f886ffa4e40c0f9b0af1c51c1d1a42
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-09-29 18:26:34 +0530

    remove unnesessary code

--- index.js
+++ index.js
@@ -18,14 +18,10 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-start();
+try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`));
+} catch (error) {
+    console.log(error);
+}
